3ace4702fc5d3afee335134c2d950eb00e2b0d26
Malik Bandara <malik.bandara@jaznu.com>
2025-12-14 12:55:45 +0530

    fixed

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -9,7 +9,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "RichWin",
   description: "Color Prediction Game",
-  viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({ children }) {
